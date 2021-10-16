b541520881c78432ec18d36e27cb7ef7e44f7802
Toni Panacek <toni.panacek@gmail.com>
2021-10-16 10:32:24 +0200

    initial setup for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'pages#home'
+  get '/members-only', to: "pages#members", as: :members_only
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
